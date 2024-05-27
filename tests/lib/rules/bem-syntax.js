a62e5b38481bf62da7e37258dbb38f88bfbef75e
Emil Murahas <murakhas@list.ru>
2024-05-27 12:51:55 +0300

    feat: add new import packages rule

--- tests/lib/rules/bem-syntax.js
+++ tests/lib/rules/bem-syntax.js
@@ -15,19 +15,58 @@ const rule = require('../../../lib/rules/bem-syntax'),
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+	parserOptions: {
+		ecmaVersion: 8,
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true,
+			experimentalObjectRestSpread: true,
+		},
+	},
+});
+
+// TODO: Дописать тесты
 ruleTester.run('bem-syntax', rule, {
 	valid: [
 		{
-			code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice'",
+			code: `const Avatar = ({ onAvatarChange, input }) => {
+				return (
+					<div className="avatar">
+						{input.value ? (
+							<img className="avatar__image" src={input.value} alt="avatar" />
+						) : (
+							<Icon size="massive" name="image outline" data-testid="icon" />
+						)}
+						<Input onChange={onAvatarChange} type="file" id="input__file" />
+						<div className="avatar__icon-wrapper">
+							<Icon className="avatar__icon" name="pencil alternate" />
+						</div>
+					</div>
+				);
+			}`.replace(/\t/g, '  '),
 			errors: [],
 		},
 	],
 
 	invalid: [
 		{
-			code: '',
-			errors: [{ message: 'Fill me in.', type: 'Me too' }],
+			code: `const Avatar = ({ onAvatarChange, input }) => {
+				return (
+					<div className="avatar_2131231212">
+						{input.value ? (
+							<img className="avatar__image" src={input.value} alt="avatar" />
+						) : (
+							<Icon size="massive" name="image outline" data-testid="icon" />
+						)}
+						<Input onChange={onAvatarChange} type="file" id="input__file" />
+						<div className="avatar__icon-wrapper">
+							<Icon className="avatar__icon" name="pencil alternate" />
+						</div>
+					</div>
+				);
+			}`.replace(/\t/g, '  '),
+			errors: [{ message: 'Наименование родительского класса и компонента не совпадают' }],
 		},
 	],
 });
---
+++ tests/lib/rules/import-packages.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview rule for disable global imports packages
+ * @author import-packages
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/import-packages'),
+	RuleTester = require('eslint').RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+	parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+});
+
+ruleTester.run('import-packages', rule, {
+	valid: [{ code: 'import { debounce } from "lodash/debounce" ', errors: [] }],
+
+	invalid: [
+		{
+			code: 'import test from "lodash" ',
+			errors: [{ message: 'Импортируйте только нужный модуль а не всю библиотеку' }],
+		},
+	],
+});
